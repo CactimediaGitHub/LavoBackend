084c74e7e2349d93c25a7e7b1d0a43345f7c1e99
Developer 5 <dev1@cactimedia.biz>
2017-04-26 18:21:08 +0530

    set default value to 050 for prefix phone number

--- db/migrate/20170426103259_add_column_prefix_number_to_customers.rb
+++ db/migrate/20170426103259_add_column_prefix_number_to_customers.rb
@@ -1,5 +1,5 @@
 class AddColumnPrefixNumberToCustomers < ActiveRecord::Migration[5.0]
   def change
-    add_column :customers, :prefix_phone, :text
+    add_column :customers, :prefix_phone, :text, default: '050'
   end
 end
