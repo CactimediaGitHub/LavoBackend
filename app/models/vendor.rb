0c6a65dd6a3b5eedadcaa3da482b345a248c2d27
Developer 5 <dev1@cactimedia.biz>
2017-08-01 19:47:53 +0530

    updated the min max value of phone number validation

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -51,7 +51,7 @@ class Vendor < ApplicationRecord
   # FIXME: set comission as separate has_many association
   validates :commission, numericality: { greater_than: 0, only_integer: true }
   validates :flat_rate, numericality: { greater_than_or_equal_to: 0, only_integer: true }
-  validates :phone, format: { with: PHONE_FORMAT_REGEXP }, length: { minimum: 7, maximum: 11 }
+  validates :phone, format: { with: PHONE_FORMAT_REGEXP }, length: { minimum: 9, maximum: 10 }
   validates :emirate, presence: true
 
   scope :activated, -> { where(activated: true) }
