3afb220db90750494f90ee898df2f804587e7e5e
Developer 5 <dev1@cactimedia.biz>
2017-08-08 17:47:34 +0530

    Added sorting by distance in search functionality

--- app/controllers/api/v1/vendor/search_controller.rb
+++ app/controllers/api/v1/vendor/search_controller.rb
@@ -1,8 +1,20 @@
 class API::V1::Vendor::SearchController < API::V1::VersionController
 
   def index
-    vendor_index = ::Index::VendorSearchIndex.new(self)
-    vendors = vendor_index.vendors(Vendor.search_by_name(Vendor.activated, params[:query]))
-    render json: vendors, meta: pagination(vendors)
+    vendors = Vendor.search_by_name(Vendor.activated, params[:query])
+    	.activated
+    	.select_with_distance(near_vendors_params[:lat], near_vendors_params[:lon])
+    
+    serialized_vendors_json = ActiveModelSerializers::SerializableResource.new(vendors).as_json
+    
+    vendors_json = Vendor.stuff_distance(vendors, serialized_vendors_json)
+    
+    render json: vendors_json
+  end
+
+  private
+
+  def near_vendors_params
+    params.permit(:lat, :lon)
   end
 end
